b6b228f7c1162864585e543bce2f7b9f626c7125
David Livingstone <d.livingstone@kingston.ac.uk>
2021-01-05 23:42:32 +0000

    Starting framework for SwingWorkshops
    This is an empty project that can be used as the starting framework for SwingWorkshops
    The initial workshop is for a GitLab submission of the readcsv kata and to add a basic viewer with a JTextArea

---
+++ SwingWorkshops/src/test/java/ku/pii2020/swingworkshops/view/Dummy.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ku.pii2020.swingworkshops.view;
+
+import ku.pii2020.swingworkshops.view.*;
+
+/**
+ *
+ * @author dave
+ * An empty class as a .gitkeep 
+ * So git keeps the otherwise empty package
+ */
+public class Dummy {
+    
+}
