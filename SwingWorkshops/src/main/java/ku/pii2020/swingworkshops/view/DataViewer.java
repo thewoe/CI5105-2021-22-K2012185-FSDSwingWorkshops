2c08a6a892a3d601eca80af83b1297e9f7e70f3b
Tug O'Flaherty <K2012185@kingston.ac.uk>
2022-01-08 20:16:40 +0000

    W2R5 - In DataViewer class, MenuBar menuBar added to JFrame. menuBar contains JMenu fileMenu and editMenu. fileMenu contains loadFile and quitViewer JMenuItem, whilst editMenu contains deleteTask JMenuItem. All buttons are duplicated in the menu options. Updated JFrame size to include space for menuBar display. Application working as intended.

--- SwingWorkshops/src/main/java/ku/pii2020/swingworkshops/view/DataViewer.java
+++ SwingWorkshops/src/main/java/ku/pii2020/swingworkshops/view/DataViewer.java
@@ -8,6 +8,10 @@ package ku.pii2020.swingworkshops.view;
 import java.awt.BorderLayout;
 import java.awt.HeadlessException;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import ku.pii2020.swingworkshops.model.MyHandler;
 
 /**
  *
@@ -17,6 +21,12 @@ public class DataViewer extends JFrame {
     // Attribute Declarations
     private static TextAreaPanel textPanel = new TextAreaPanel();
     private static ButtonPanel buttonPanel = new ButtonPanel();
+    private static JMenuBar menuBar = new JMenuBar();
+    private static JMenu fileMenu = new JMenu("File");
+    private static JMenu editMenu = new JMenu("Edit");
+    private static JMenuItem loadFile = new JMenuItem("Load File");
+    private static JMenuItem quitViewer = new JMenuItem("Quit");
+    private static JMenuItem deleteTask = new JMenuItem("Delete Task");
     
     // Attribute Getter and Setter Methods    
     public static TextAreaPanel getTextPanel() {
@@ -34,15 +44,71 @@ public class DataViewer extends JFrame {
     public static void setButtonPanel(ButtonPanel buttonPanel) {
         DataViewer.buttonPanel = buttonPanel;
     }
+
+    public static void setMenuBar(JMenuBar menuBar) {
+        DataViewer.menuBar = menuBar;
+    }
+
+    public static JMenu getFileMenu() {
+        return fileMenu;
+    }
+
+    public static void setFileMenu(JMenu fileMenu) {
+        DataViewer.fileMenu = fileMenu;
+    }
+
+    public static JMenu getEditMenu() {
+        return editMenu;
+    }
+
+    public static void setEditMenu(JMenu editMenu) {
+        DataViewer.editMenu = editMenu;
+    }
+
+    public static JMenuItem getLoadFile() {
+        return loadFile;
+    }
+
+    public static void setLoadFile(JMenuItem loadFile) {
+        DataViewer.loadFile = loadFile;
+    }
+
+    public static JMenuItem getQuitViewer() {
+        return quitViewer;
+    }
+
+    public static void setQuitViewer(JMenuItem quitViewer) {
+        DataViewer.quitViewer = quitViewer;
+    }
+
+    public static JMenuItem getDeleteTask() {
+        return deleteTask;
+    }
+
+    public static void setDeleteTask(JMenuItem deleteTask) {
+        DataViewer.deleteTask = deleteTask;
+    }
     
     // Constructor Methods
     public DataViewer() throws HeadlessException {
         this.setTitle("My Task List");
-        this.setBounds(400,0,400,400);
+        this.setBounds(400,0,400,425);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
         this.setLayout(new BorderLayout());
         this.add(DataViewer.getTextPanel(), BorderLayout.CENTER);
         this.add(DataViewer.getButtonPanel(), BorderLayout.SOUTH);
+        DataViewer.loadFile.setActionCommand("Load File");
+        DataViewer.loadFile.addActionListener(new MyHandler());
+        DataViewer.quitViewer.setActionCommand("Quit");
+        DataViewer.quitViewer.addActionListener(new MyHandler());
+        DataViewer.deleteTask.setActionCommand("Delete Task");
+        DataViewer.deleteTask.addActionListener(new MyHandler());
+        DataViewer.fileMenu.add(loadFile);
+        DataViewer.fileMenu.add(quitViewer);
+        DataViewer.editMenu.add(deleteTask);
+        DataViewer.menuBar.add(fileMenu);
+        DataViewer.menuBar.add(editMenu);
+        this.add(menuBar, BorderLayout.NORTH);
     }
 }
